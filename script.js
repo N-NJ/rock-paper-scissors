953aade0665b0e6e3c78ce289e5e99b181f2c73a
N-NJ <171683125+N-NJ@users.noreply.github.com>
2024-07-24 15:49:59 +0300

    added logic for cancelling game

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 let choices = ["rock", "paper", "scissors"];
+let humanScore = 0,
+  computerScore = 0;
+console.log(humanScore, computerScore);
 function getComputerChoice() {
   let choice = Math.floor(Math.random() * 3);
   return choices[choice];
@@ -6,6 +9,9 @@ function getComputerChoice() {
 function getHumanChoice() {
   let keepGoing = true;
   let choice = prompt("Please Write your choice: rock, paper, scissors");
+  if (choice === null) {
+    return "Game Cancelled, please refresh the page to play again";
+  }
   while (keepGoing) {
     if (
       choice.toLowerCase() === "rock" ||
@@ -17,6 +23,7 @@ function getHumanChoice() {
       choice = prompt("Please enter a valid choice (rock, paper, scissors)");
     }
   }
+
   return choice.toLowerCase();
 }
 console.log(getHumanChoice());
