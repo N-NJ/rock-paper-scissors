8d62d423831f472085ac16eeea78aafd25371460
N-NJ <171683125+N-NJ@users.noreply.github.com>
2024-07-24 15:44:03 +0300

    Human Choice logic done

--- script.js
+++ script.js
@@ -3,4 +3,20 @@ function getComputerChoice() {
   let choice = Math.floor(Math.random() * 3);
   return choices[choice];
 }
-console.log(getComputerChoice());
+function getHumanChoice() {
+  let keepGoing = true;
+  let choice = prompt("Please Write your choice: rock, paper, scissors");
+  while (keepGoing) {
+    if (
+      choice.toLowerCase() === "rock" ||
+      choice.toLowerCase() === "scissors" ||
+      choice.toLowerCase() === "paper"
+    ) {
+      keepGoing = false;
+    } else {
+      choice = prompt("Please enter a valid choice (rock, paper, scissors)");
+    }
+  }
+  return choice.toLowerCase();
+}
+console.log(getHumanChoice());
