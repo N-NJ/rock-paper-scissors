e0598c9536056d94f96eb6326e896bc05e5d8a51
N-NJ <171683125+N-NJ@users.noreply.github.com>
2024-07-25 12:30:29 +0300

    finished game, as console interface only

--- script.js
+++ script.js
@@ -1,23 +1,20 @@
+//Computer choosing based on numbers & array item for each number
 let choices = ["rock", "paper", "scissors"];
 
 function getComputerChoice() {
   let choice = Math.floor(Math.random() * 3);
   return choices[choice];
 }
+// Human choice checking for null and returning choice as lowercase
 function getHumanChoice() {
   let keepGoing = true;
   let choice = prompt("Please Write your choice: rock, paper, scissors");
   if (choice === null) {
-    return console.log(
-      "Game Cancelled, please refresh the page to play again."
-    );
+    return choice;
   }
   while (keepGoing) {
-    if (
-      choice.toLowerCase() === "rock" ||
-      choice.toLowerCase() === "scissors" ||
-      choice.toLowerCase() === "paper"
-    ) {
+    choice = choice.toLowerCase();
+    if (choice === "rock" || choice === "scissors" || choice === "paper") {
       keepGoing = false;
     } else {
       choice = prompt("Please enter a valid choice (rock, paper, scissors)");
@@ -26,18 +23,20 @@ function getHumanChoice() {
 
   return choice.toLowerCase();
 }
-
+//Game logic has round function in it, break loop if player cancelled a round
 function playGame() {
   let humanScore = 0,
     computerScore = 0;
   let n = 1;
   function playRound(humanChoice, computerChoice) {
+    //checking for tie
     if (humanChoice === computerChoice) {
       console.log(`It's a tie! You both chose ${humanChoice}`);
       humanScore++;
       computerScore++;
       return humanScore, computerChoice;
     }
+    //switch case checking for all lose conditions if not valid => it's a win.
     switch (true) {
       case humanChoice === `rock` && computerChoice === `paper`:
       case humanChoice === `paper` && computerChoice === `scissors`:
@@ -45,28 +44,39 @@ function playGame() {
         computerScore++;
         console.log(`You lose! ${computerChoice} beats ${humanChoice}!`);
         break;
+      //default case of winning
       default:
         humanScore++;
         console.log(`You win! ${humanChoice} beats ${computerChoice}!`);
     }
     return humanScore, computerScore;
   }
+  //looping for 5 rounds
   while (n <= 5) {
     const humanChoice = getHumanChoice();
     const computerChoice = getComputerChoice();
-    playRound(humanChoice, computerChoice);
-    console.log(humanScore, computerScore);
-    n++;
+    //checking if round cancelled, cancel game by setting n > 5, set humanScore to null to avoid game tie condition
+    if (humanChoice === null) {
+      console.log("Game Cancelled, please refresh the page to play again.");
+      n = 6;
+      humanScore = null;
+    } else {
+      playRound(humanChoice, computerChoice);
+      console.log(humanScore, computerScore);
+      n++;
+    }
   }
+  //announcing final score by comparing humanScore to computerScore
   if (humanScore > computerScore) {
     console.log(`You win the game!`);
   } else if (computerScore > humanScore) {
     console.log(`You lose the game, better luck next time!`);
-  } else {
+  } else if (humanScore === computerScore) {
     console.log(`The game's a tie!`);
   }
 }
 playGame();
+//Loop to restart game based on user choice to confirm.
 let keepGoing = true;
 while (keepGoing) {
   if (confirm(`Play again?`)) {
